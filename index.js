dd5411d4dd10ef2ad355841e7d40069e6779cc2a
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-04-07 01:57:04 +0900

    빌드 에러 수정

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/", (req, res) => {
-  res.send("hello");
-});
 app.use("/user", userRouter);
 app.use("/board", boardRouter);
 app.use("/upload", uploadRouter);
