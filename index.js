23e2d403470eac5f13da02692bfc9eff971c911e
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-04-12 12:21:50 +0900

    cors 적용

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import alertRouter from "./routes/alert.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({ origin: "https://comboard.netlify.app" }));
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("mongodb connected"))
