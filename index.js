8b1b5fb45831ada0b132833e358048cc235ef3ad
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-04-12 12:01:23 +0900

    에러 수정

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import alertRouter from "./routes/alert.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+// app.use(cors());
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("mongodb connected"))
